116b48ed4ceeae14aa756844b4da42c2eccfda7f
Richard Neumann <r.neumann@homeinfo.de>
2021-12-19 14:45:11 +0100

    Add properties parser

---
+++ pymcadmin/properties.py
@@ -0,0 +1,71 @@
+"""Properties parser."""
+
+from configparser import ConfigParser
+from io import StringIO
+from tempfile import NamedTemporaryFile
+from typing import Any, IO, Iterable, Union, Optional
+
+
+__all__ = ['PropertiesParser']
+
+
+PSEUDO_SECTION = 'properties'
+
+
+class _ConfigParser(ConfigParser):
+    """Modified Configparser for reading server.properties."""
+
+    def _read(self, fp: IO, fpname: str) -> None:
+        """Reads a single file."""
+        return super()._read(StringIO(f'[{PSEUDO_SECTION}]\n{fp.read()}'),
+                             fpname)
+
+
+class PropertiesParser:
+    """Parse Java properties files."""
+
+    def __init__(self):
+        self.parser = _ConfigParser()
+
+    def __getitem__(self, key: str) -> str:
+        return self.parser[PSEUDO_SECTION][key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.parser[PSEUDO_SECTION][key] = value
+
+    def get(self, key: str, **kwargs) -> str:
+        """Returns the value of the respective key."""
+        return self.parser.get(PSEUDO_SECTION, key, **kwargs)
+
+    def getint(self, key: str, **kwargs) -> int:
+        """Returns an integer value."""
+        return self.parser.getint(PSEUDO_SECTION, key, **kwargs)
+
+    def getfloat(self, key: str, **kwargs) -> float:
+        """Returns an float value."""
+        return self.parser.getfloat(PSEUDO_SECTION, key, **kwargs)
+
+    def getboolean(self, key: str, **kwargs) -> bool:
+        """Returns an boolean value."""
+        return self.parser.getboolean(PSEUDO_SECTION, key, **kwargs)
+
+    def set(self, key: str, value: Optional[Any] = None) -> None:
+        """Sets a value."""
+        return self.parser.set(PSEUDO_SECTION, key, value)
+
+    def read(self, filenames: Union[str, Iterable[str]],
+             encoding: Optional[str] = None) -> list[str]:
+        """Reads files."""
+        return self.parser.read(filenames, encoding=encoding)
+
+    def write(self, filename: str) -> None:
+        """Writes to the given file."""
+        with NamedTemporaryFile('w+') as tmp:
+            self.parser.write(tmp, space_around_delimiters=False)
+            tmp.flush()
+            tmp.seek(0)
+
+            with open(filename, 'w', encoding='utf-8') as file:
+                for index, line in enumerate(tmp):
+                    if index > 0:   # Skip section header.
+                        file.write(line)
