5e8f762d4bca6ce34bfe251ac7910522e1b3093e
Richard Neumann <r.neumann@homeinfo.de>
2021-12-19 14:46:51 +0100

    Rename default section

--- pymcadmin/properties.py
+++ pymcadmin/properties.py
@@ -9,7 +9,7 @@ from typing import Any, IO, Iterable, Union, Optional
 __all__ = ['PropertiesParser']
 
 
-PSEUDO_SECTION = 'properties'
+SECTION = 'properties'
 
 
 class _ConfigParser(ConfigParser):
@@ -17,8 +17,7 @@ class _ConfigParser(ConfigParser):
 
     def _read(self, fp: IO, fpname: str) -> None:
         """Reads a single file."""
-        return super()._read(StringIO(f'[{PSEUDO_SECTION}]\n{fp.read()}'),
-                             fpname)
+        return super()._read(StringIO(f'[{SECTION}]\n{fp.read()}'), fpname)
 
 
 class PropertiesParser:
@@ -28,30 +27,30 @@ class PropertiesParser:
         self.parser = _ConfigParser()
 
     def __getitem__(self, key: str) -> str:
-        return self.parser[PSEUDO_SECTION][key]
+        return self.parser[SECTION][key]
 
     def __setitem__(self, key: str, value: Any) -> None:
-        self.parser[PSEUDO_SECTION][key] = value
+        self.parser[SECTION][key] = value
 
     def get(self, key: str, **kwargs) -> str:
         """Returns the value of the respective key."""
-        return self.parser.get(PSEUDO_SECTION, key, **kwargs)
+        return self.parser.get(SECTION, key, **kwargs)
 
     def getint(self, key: str, **kwargs) -> int:
         """Returns an integer value."""
-        return self.parser.getint(PSEUDO_SECTION, key, **kwargs)
+        return self.parser.getint(SECTION, key, **kwargs)
 
     def getfloat(self, key: str, **kwargs) -> float:
         """Returns an float value."""
-        return self.parser.getfloat(PSEUDO_SECTION, key, **kwargs)
+        return self.parser.getfloat(SECTION, key, **kwargs)
 
     def getboolean(self, key: str, **kwargs) -> bool:
         """Returns an boolean value."""
-        return self.parser.getboolean(PSEUDO_SECTION, key, **kwargs)
+        return self.parser.getboolean(SECTION, key, **kwargs)
 
     def set(self, key: str, value: Optional[Any] = None) -> None:
         """Sets a value."""
-        return self.parser.set(PSEUDO_SECTION, key, value)
+        return self.parser.set(SECTION, key, value)
 
     def read(self, filenames: Union[str, Iterable[str]],
              encoding: Optional[str] = None) -> list[str]:
